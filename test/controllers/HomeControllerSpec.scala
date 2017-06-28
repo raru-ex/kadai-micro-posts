917c2ba5cf9e5337ad99c244f0a55a75cf3d85a4
藪崎卓巳 <raru@syrup16g.net>
2017-06-29 01:55:40 +0900

    add top page

---
+++ test/controllers/HomeControllerSpec.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+/**
+  * Created by raru on 2017/06/29.
+  */
+class HomeControllerSpec extends PlayFunSpec with GuiceOneAppPerSuite {
+
+  describe("HomeController") {
+    describe("route of HomeController#index") {
+      it("should be valid") {
+        val result = route(app, addCsrfToken(FakeRequest(GET, routes.HomeController.index().toString)))
+      }
+    }
+  }
+}
